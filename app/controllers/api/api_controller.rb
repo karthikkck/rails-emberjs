6991c403959a93da2b7e2dc22f64734bd0e6fa4e
karthik k c <karthikkck@gmail.com>
2012-05-29 09:24:38 +0530

    users controller created

--- app/controllers/api/api_controller.rb
+++ app/controllers/api/api_controller.rb
@@ -1,7 +1,34 @@
 class Api::ApiController < ApplicationController
+  before_filter :authenticate_request, :except => [:render_404]
   before_filter :set_default_format
+  helper_method :current_user, :request_user
+
+  def render_errors(template = "api/#{controller_name}/errors", errors = [], status = :unprocessable_entity)
+    @errors = errors unless errors.blank?
+    template = 'api/shared/errors' unless template_exists?("errors", controller_name, false)
+
+    render :json, template: template, status: status
+  end
 
   private
+
+  def current_user
+    return nil unless @authentication_token.present?
+    @current_user ||= User.find_by_authentication_token(@authentication_token)
+  end
+
+  def request_user
+    return current_user unless params[:user_id].present?
+    @request_user ||= User.find_by_id(params[:user_id])
+    render_errors('api/shared/errors', ['users.not_found']) unless @request_user.present?
+    @request_user
+  end
+
+  def authenticate_request
+    @authentication_token = params[:authentication_token]
+    render_errors('api/shared/errors', ['users.not_authenticated'], :unauthorized) unless current_user.present?
+  end
+
   def set_default_format
     request.format = "json" unless params[:format]
   end
---
+++ app/controllers/api/users_controller.rb
@@ -0,0 +1,42 @@
+class Api::UsersController < Api::ApiController
+
+  skip_before_filter :authenticate_request, :only => [:forgot_password, :create, :sign_in]
+
+  def show
+    @user = User.find(params[:id])
+  end
+
+  def create
+    @user = User.new(params[:user])
+    unless @user.save
+      render_errors('api/shared/errors',@user.errors)
+    else
+      respond_to do |format|
+        format.json { render :json=> {message: 'successful'}.to_json }
+      end
+    end
+  end
+
+  def forgot_password
+    @user = User.find_by_email(params[:email])
+    unless @user.present?
+      render_errors('api/shared/errors', 'Email id does not exist')
+    else
+      respond_to do |format|
+        format.json { render :json=> {message: 'Check your email account for changing password'}.to_json }
+      end
+    end
+  end
+
+  def sign_in
+    @user = User.find_by_email(params[:email])
+    unless @user.present? && @user.authenticate(params[:password])
+      render_errors('api/shared/errors', 'Username and password do not match')
+    else
+      respond_to do |format|
+        format.json { render :json=> {message: 'Login successful'}.to_json }
+      end
+    end
+  end
+
+end
